84354645f0ae076ba480dff1161e4761fce83858
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2021-08-23 19:23:36 +0100

    manage admin

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -6,8 +6,67 @@
     <div class="main-content">
 
         <div class="wrapper">
+            
 
             <h2>Manage Admin</h2>
+
+            <!-- Button to Add Admin-->
+
+            <br />
+
+            <a href="#" class="btn-primary">Add Admin</a>
+
+            <br />
+            <br />
+
+            <table class="tbl-full">
+
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+
+                    <td>1.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                       <a href="#" class="btn-secondary">Update Admin</a>
+                       <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+
+                <tr>
+
+                    <td>2.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+
+                <tr>
+
+                    <td>3.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+            </table>
            
         </div>
         
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -6,6 +6,62 @@
 
             <h2>Manage Foods</h2>
 
+            <br />
+
+            <a href="#" class="btn-primary">Add Food</a>
+
+            <br />
+            <br />
+
+            <table class="tbl-full">
+
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+
+                    <td>1.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                       <a href="#" class="btn-secondary">Update Admin</a>
+                       <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+
+                <tr>
+
+                    <td>2.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+
+                <tr>
+
+                    <td>3.</td>
+                    <td>Lucky Benjamin</td>
+                    <td>LuckyBenjamin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+
+                </tr>
+
+            </table>
+
         </div>
 
 
